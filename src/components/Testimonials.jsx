e2aecf4495a4384011d4b91f0f783eda9d1a1f7d
JiyaGupta-cs <140608790+JiyaGupta-cs@users.noreply.github.com>
2024-07-02 23:47:59 +0530

    changes

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,12 +1,15 @@
 import { feedback } from "../constants";
 import styles from "../style";
 import FeedbackCard from "./FeedbackCard";
-
+import naruto from '../assets/naruto.png'
+import naruto2 from '../assets/naruto2.png'
+import gm from '../assets/gm.jpg'
+import gn from '../assets/gn.jpg'
 const Testimonials = () => (
   <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}>
     <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
 
-    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+    {/* <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
       <h2 className={styles.heading2}>
         What People are <br className="sm:block hidden" /> saying about us
       </h2>
@@ -16,10 +19,18 @@ const Testimonials = () => (
           anywhere on the planet.
         </p>
       </div>
-    </div>
+    </div> */}
 
     <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
-      {feedback.map((card) => <FeedbackCard key={card.id} {...card} />)}
+      {/* {feedback.map((card) => <FeedbackCard key={card.id} {...card} />)} */}
+    </div>
+    <div className="flex gap-8 my-3 mb-8">
+<img src={gm} className="rounded-lg w-[30vw]" alt="" />
+<img src={gn} className="rounded-lg w-[30vw]" alt="" />
+    </div>
+    <div className="flex gap-8">
+<img src={naruto} className="rounded-lg w-[30vw]" alt="" />
+<img src={naruto2} className="rounded-lg w-[30vw]" alt="" />
     </div>
   </section>
 );
